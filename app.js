dbffaba7c00a83985db8067abad4b0ae9e599a9c
Janet Malavet <J_rm78@yahoo.com>
2023-03-26 20:46:24 -0400

    new files

---
+++ app.js
@@ -0,0 +1,63 @@
+import java.util.HashMap; 
+import java.util.Map;
+import java.util.Scanner; 
+public class WordOccurrences {
+
+   public static void main(String[] args) { 
+       String text = ("Once upon a midnight dreary, while I pondered, weak and weary,\r\n"
+       		+ "Over many a quaint and curious volume of forgotten lore\r\n"
+       		+ "While I nodded, nearly napping, suddenly there came a tapping,\r\n"
+       		+ "As of some one gently rapping, rapping at my chamber door.\r\n"
+       		+ "Tis some visiter, I muttered, tapping at my chamber door\r\n"
+       		+ "Only this and nothing more.\r\n"
+       		+ "");
+     				
+  			         		      	
+			   String[] words = text.split(" "); 
+			   Map<String, Integer> counts = new HashMap<String, Integer>(); 
+			   for (String word : words) { 
+			       Integer count = counts.get(word); 
+			       if (count == null) {  
+			           counts.put(word, 1); 
+			       }
+			       else  {
+			           counts.put(word, count + 1); 
+			       }
+			       }
+			   
+			   System.out.println("Word Occurrences:  " + counts); 
+   }
+}
+
+package testing;
+ 
+import static org.junit.Assert.*; 
+import org.junit.Test; 
+import java.util.HashMap; 
+import java.util.Map; 
+public class WordOccurrencesTest {
+
+   @Test 
+   public void testWordOccurrences() { 
+       String text = ("Once upon a midnight dreary, while I pondered, weak and weary,\r\n"
+          		+ "Over many a quaint and curious volume of forgotten lore\r\n"
+           		+ "While I nodded, nearly napping, suddenly there came a tapping,\r\n"
+           		+ "As of some one gently rapping, rapping at my chamber door.\r\n"
+           		+ "Tis some visiter, I muttered, tapping at my chamber door\r\n"
+           		+ "Only this and nothing more.\r\n"
+           		+ "");
+       		       String[] words = text.split(" "); 
+       Map<String, Integer> counts = new HashMap<String, Integer>(); 
+       for (String word : words) { 
+           Integer count = counts.get(word); 
+           if (count == null) { 
+               counts.put(word, 1); 
+           } 
+           else { 
+               counts.put(word, count + 1); 
+           } 
+       } 
+       assertEquals(counts, WordOccurrences.main(words)); 
+   }
+
+}
